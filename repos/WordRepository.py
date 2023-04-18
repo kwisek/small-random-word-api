c17e2505c5d367dbbc9112334a8d15fe9f4050ac
kwisek <50746323+kwisek@users.noreply.github.com>
2023-04-18 15:10:33 +0200

    Exception handling

--- repos/WordRepository.py
+++ repos/WordRepository.py
@@ -2,29 +2,43 @@ import sqlite3
 import random
 
 def all_words():
-    connection = sqlite3.connect('data/words.db')
-    words = connection.execute(f"SELECT CONTENT FROM WORD;").fetchall()
-    connection.close()
-    return list(map(lambda x: x[0], words))
+    try:
+        connection = sqlite3.connect('data/words.db')
+        words = connection.execute(f"SELECT CONTENT FROM WORD;").fetchall()
+        connection.close()
+        return list(map(lambda x: x[0], words))
+    except:
+        return []
+
+    
 
 def all_words_by_len(len: int):
-    connection = sqlite3.connect('data/words.db')
-    if (len is None):
-        words = connection.execute(f"SELECT CONTENT FROM WORD;").fetchall()
-    else:
-        words = connection.execute(f"SELECT CONTENT, LENGTH(CONTENT) FROM WORD WHERE LENGTH(CONTENT) = {len};").fetchall()
-    connection.close()
-    return list(map(lambda x: x[0], words))
+    try:
+        connection = sqlite3.connect('data/words.db')
+        if (len is None):
+            words = connection.execute(f"SELECT CONTENT FROM WORD;").fetchall()
+        else:
+            words = connection.execute(f"SELECT CONTENT, LENGTH(CONTENT) FROM WORD WHERE LENGTH(CONTENT) = {len};").fetchall()
+        connection.close()
+        return list(map(lambda x: x[0], words))
+    except:
+        return []
+    
 
 def all_words_by_pattern(pattern: str):
-    pattern = pattern.replace("~", "%")
-    connection = sqlite3.connect('data/words.db')
-    words = connection.execute(f"SELECT CONTENT FROM WORD WHERE CONTENT LIKE '{pattern}';").fetchall()
-    connection.close()
-    return list(map(lambda x: x[0], words))
+    try:
+        pattern = pattern.replace("~", "%")
+        connection = sqlite3.connect('data/words.db')
+        words = connection.execute(f"SELECT CONTENT FROM WORD WHERE CONTENT LIKE '{pattern}';").fetchall()
+        connection.close()
+        return list(map(lambda x: x[0], words))
+    except:
+        return []
 
 def random_word_by_len(len: int):
-    return random.choice(all_words_by_len(len))
+    result = all_words_by_len(len)
+    return "" if not result else random.choice(result)
 
 def random_word_by_pattern(pattern: str):
-    return random.choice(all_words_by_pattern(pattern))
+    result = all_words_by_pattern(pattern)
+    return "" if not result else random.choice(result)
